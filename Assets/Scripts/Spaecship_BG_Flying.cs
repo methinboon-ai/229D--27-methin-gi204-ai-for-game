e54d59e50571faee3945fd783d79e6b06ee6ad94
methinboon-ai <methin.boon@bumail.net>
2026-03-09 13:40:27 +0700

    moving spaceship

---
+++ Assets/Scripts/Spaecship_BG_Flying.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Spaecship_BG_Flying : MonoBehaviour
+{
+    void Start()
+    {
+        ConstantForce force = GetComponent<ConstantForce>();
+        force.relativeForce = new Vector3(0, 0, 10);
+        force.relativeTorque = new Vector3(0, Random.Range(-100,100), 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
